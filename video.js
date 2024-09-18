a8f1ec00a48957a365063f2f054d3379d339397d
Jason Bertschi <jabertsc@microsoft.com>
2024-09-18 16:24:13 -0400

    Updates to preview (centering)

--- video.js
+++ video.js
@@ -1,10 +1,12 @@
+const size = 128;
+
 var video = document.createElement('video');
 /* var video = document.getElementById('video'); */
 video.setAttribute('playsinline', '');
 video.setAttribute('autoplay', '');
 video.setAttribute('muted', '');
-video.style.width = '200px';
-video.style.height = '200px';
+video.style.width = `${size}px`;
+video.style.height = `${size}px`;
 
 /* Setting up the constraint */
 var facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
@@ -21,35 +23,32 @@ navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
 });
 
 var canvas = document.createElement("canvas");
-canvas.style.width = '200px';
-canvas.style.height = '200px';
+canvas.style.width = `${size}px`;
+canvas.style.height = `${size}px`;
 
 var text = document.getElementById('text')
 
 var snap = () => {
+    var ctx = preview.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, size, size);
+
     var vw = video.videoWidth
     var vh = video.videoHeight
-    var w = 200;
-    var h = 200;
+    var w = size;
+    var h = size;
+    var xoff = 0;
+    var yoff = 0;
     if (vw > vh) {
-        h = 200 * vh / vw;
+        h = size * vh / vw;
+        yoff = (size - h) / 2;
     } else {
-        w = 200 * vw / vh;
+        w = size * vw / vh;
+        xoff = (size - w) / 2;
     }
-//  canvas.getContext('2d').drawImage(video, 0, 0, w, h);
-//  preview.getContext('2d').drawImage(canvas, 0, 0, 200, 200);
-    preview.getContext('2d').drawImage(video, 0, 0, w, h);
-//    text.innerHTML = `${vw}x${vh}, ${w}x${h}`;
-//    text.innerHTML = 'test3';
-//    console.log('here3')
+    preview.getContext('2d').drawImage(video, xoff, yoff, w, h);
     preview.toBlob((blob) => {
-//      const url = URL.createObjectURL(blob);
-//        text.innerHTML = 'test2';
-//        console.log('here')
         blob.text().then((blobtext) => {
-//            text.innerHTML = 'test';
-//            console.log('here2');
-//            console.log(blobtext);
         });
     });
 }
